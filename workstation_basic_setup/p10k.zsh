7d8bcef8f1d2687e710d81d589fa4fb915a786c5
frank03k <63907798+frank03k@users.noreply.github.com>
2021-09-22 08:42:47 +0200

    Update p10k.zsh

--- workstation_basic_setup/p10k.zsh
+++ workstation_basic_setup/p10k.zsh
@@ -714,7 +714,7 @@
   # Ranger shell color.
   typeset -g POWERLEVEL9K_RANGER_FOREGROUND=178
   # Custom icon.
-  # typeset -g POWERLEVEL9K_RANGER_VISUAL_IDENTIFIER_EXPANSION='⭐'
+  # typeset -g POWERLEVEL9K_RANGER_VISUAL_IDENTIFIER_EXPANSION=''
 
   ######################[ nnn: nnn shell (https://github.com/jarun/nnn) ]#######################
   # Nnn shell color.
